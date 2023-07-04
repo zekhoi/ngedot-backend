078e414836125661fc81ef275aaab154f7ecccfa
khoironi <khoironi@zero-one-group.com>
2023-07-05 01:24:20 +0800

    test: update test with logged in user

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -7,53 +7,78 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Province;
 use App\Models\City;
+use App\Models\User;
 
 class ApiTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_api_search_all_provinces(): void
+    public function test_api_search_all_provinces_as_user(): void
     {
+        $user = User::factory()->create();
+        
         $provinces = Province::all();
+        
+        $this->actingAs($user);
         $response = $this->get('/search/provinces');
 
         $response->assertStatus(200);
         $response->assertJson([
             'data' => $provinces->toArray(),
         ]);
+
+        $user->delete();
     }
 
-    public function test_api_search_provinces_by_id(): void
+    public function test_api_search_provinces_by_id_as_user(): void
     {
+        $user = User::factory()->create();
+
         $province = Province::first();
+
+        $this->actingAs($user);
         $response = $this->get('/search/provinces?id=' . $province->id);
 
         $response->assertStatus(200);
         $response->assertJson([
             'data' => $province->toArray(),
         ]);
+
+        $user->delete();
     }
 
-    public function test_api_search_all_cities(): void
+    public function test_api_search_all_cities_as_user(): void
     {
+        $user = User::factory()->create();
+
         $cities = City::all();
+
+        $this->actingAs($user);
         $response = $this->get('/search/cities');
 
         $response->assertStatus(200);
         $response->assertJson([
             'data' => $cities->toArray(),
         ]);
+
+        $user->delete();
     }
 
-    public function test_api_search_cities_by_id(): void
+    public function test_api_search_cities_by_id_as_user(): void
     {
+        $user = User::factory()->create();
+
         $city = City::first();
+
+        $this->actingAs($user);
         $response = $this->get('/search/cities?id=' . $city->id);
 
         $response->assertStatus(200);
         $response->assertJson([
             'data' => $city->toArray(),
         ]);
+
+        $user->delete();
     }
 }
